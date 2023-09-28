b7fe1c2b863e046e721d3953fc537c25ab9ba9e4
noetic <noetic@noetic.myguest.virtualbox.org>
2023-09-28 13:45:36 +0800

    提交说明

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/noetic/demo05_ws/src
+# Build directory: /home/noetic/demo05_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("nav_demo")
+subdirs("urdf02_gazebo")
